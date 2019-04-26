3f9858e339ef73684494e71af77228f187b226ac
Jai Dev <35428869+Jai-kishan@users.noreply.github.com>
2019-04-26 10:35:42 +0530

    Update calling_an_api_1.py

--- Saral_Request_API/calling_an_api_1.py
+++ Saral_Request_API/calling_an_api_1.py
@@ -1,8 +1,11 @@
 #Requests is a Python HTTP library, released under the Apache2 License.
 # The goal of the project is to make HTTP requests simpler and more human-friendly.
 import requests
+#pprint — Data pretty printer. 
+#The pprint module provides a capability to “pretty-print” arbitrary Python data structures in a form which can be used as input to the interpreter.
+from pprint import pprint
 
-saral_url=("http://saral.navgurukul.org/api/courses")  //# Global variable storing api
+saral_url=("http://saral.navgurukul.org/api/courses")  # Global variable storing api
 def request(url):
 	#GET is by far the most used HTTP method. We can use GET request to retrieve data from any destination
 	response=requests.get(url)
@@ -13,4 +16,4 @@ def request(url):
 	return response.json()
 
 #Calling request()
-print(request(saral_url))
+pprint(request(saral_url))
